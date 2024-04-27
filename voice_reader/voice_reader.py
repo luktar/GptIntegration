b42823af76a5ba3d41bab23022438fb04cadbfce
Vistyy <kaszuraszymon@gmail.com>
2024-04-27 17:05:50 +0200

    add a background listener and move transcription to an instance array

--- voice_reader/voice_reader.py
+++ voice_reader/voice_reader.py
@@ -1,4 +1,5 @@
 import io
+from time import sleep
 import speech_recognition as sr
 from openai import OpenAI
 
@@ -7,15 +8,12 @@ class VoiceReader:
     def __init__(self) -> None:
         self.client = OpenAI()
         self.r = sr.Recognizer()
+        self.transcriptions = []
 
-    def record_voice(self):
-        with sr.Microphone() as source:
-            print("Calibrating...")
-            self.r.adjust_for_ambient_noise(source)
-            print("Listening...")
+    def callback(self, recognizer, audio):
+        try:
             # read the audio data from the default microphone
-            audio_data = self.r.listen(source, 4)
-            wav_data = audio_data.get_wav_data()
+            wav_data = audio.get_wav_data()
             buffer = io.BytesIO(wav_data)
             buffer.name = "recording.wav"
             print("Recognizing...")
@@ -24,5 +22,30 @@ class VoiceReader:
                 file=buffer,
                 response_format="text"
             )
-            print(transcription)
-            return transcription
+            self.transcriptions.append(transcription)
+        except sr.UnknownValueError:
+            print("Whisper Recognition could not understand audio")
+        except sr.RequestError as e:
+            print(
+                "Could not request results from Whisper Recognition service; {0}".format(e))
+
+    def record_voice(self):
+        with sr.Microphone() as source:
+            print("Calibrating...")
+            self.r.adjust_for_ambient_noise(source, 1)
+        print("Listening...")
+        stop_listening = self.r.listen_in_background(source, self.callback)
+
+        # stop_listening(wait_for_stop=True)
+        listen = True
+        while listen:
+            if len(self.get_transcriptions()) > 0:
+                print(self.get_transcriptions())
+                if 'stop' in self.get_transcriptions()[-1].lower():
+                    stop_listening(wait_for_stop=True)
+                    listen = False
+            sleep(0.1)
+
+    def get_transcriptions(self):
+        # after calling record_voice, use this to get transcriptions
+        return self.transcriptions
