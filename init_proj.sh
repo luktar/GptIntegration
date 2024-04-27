d10818b8c3af93875ec2496c79aa73c4a1d9f470
Vistyy <kaszuraszymon@gmail.com>
2024-04-27 14:35:38 +0200

    fix script

--- init_proj.sh
+++ init_proj.sh
@@ -1,2 +1,15 @@
+#!/usr/bin/env bash
+
+# Update Homebrew recipes
+echo "Updating brew recipes..."
+brew update
+
+# Install ffmpeg
+echo "Installing ffmpeg..."
 brew install ffmpeg
+
+# Install portaudio
+echo "Installing portaudio..."
 brew install portaudio
+
+echo "Installation completed."
