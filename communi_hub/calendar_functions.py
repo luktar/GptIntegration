c5c03b4e7720d7b68bde72f1f7d4f6d9bc2292ff
Krzysztof Kokot <krzysztofkokot@Krzysztofs-MacBook-Air.local>
2024-04-27 14:26:35 +0200

    declare two funcitons add, del

--- communi_hub/calendar_functions.py
+++ communi_hub/calendar_functions.py
@@ -10,12 +10,30 @@ functions = [{
                             "type": "string",
                             "description": "Appointment title",
                         },
-                        "date": {
+                        "appointment_date": {
                             "type": "string",
                             "description": "The start date for the appointment in YYYY-MM-DD format",
                         }
                     },
-                    "required": ["appointment_title", "date"],
+                    "required": ["appointment_title", "appointment_date"],
+                },
+            },
+            "function": {
+                "name": "del_appoinment_from_calendar",
+                "description": "Add appointment to the calendar",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "appointment_title": {
+                            "type": "string",
+                            "description": "Appointment title",
+                        },
+                        "appointment_date": {
+                            "type": "string",
+                            "description": "The start date for the appointment in YYYY-MM-DD format",
+                        }
+                    },
+                    "required": ["appointment_title", "appointment_date"],
                 },
             },
 }]
