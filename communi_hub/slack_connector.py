63295651a722f95fb5d1ce43f50947c044ada21d
Lukasz Taraszka <luktar@gmail.com>
2024-04-27 14:47:11 +0200

    Started working on slack integration.

--- communi_hub/slack_connector.py
+++ communi_hub/slack_connector.py
@@ -1,6 +1,11 @@
+import sys
+import logging
+from slack_sdk import WebClient
+
 class SlackConnector:
     def __init__(self) -> None:
-        pass
+        logging.basicConfig(level=logging.DEBUG)
+        client = WebClient()
 
     def send_message(self, message, receiver_name, receiver_surname) -> bool:
         return 'Slack message sent: ' + message + " receiver_name: " + receiver_name + " receiver_surname: " + receiver_surname
