fa12430b6f1b436ec87b0745127f05e146b7f5f8
Lukasz Taraszka <luktar@gmail.com>
2024-04-27 16:28:07 +0200

    Added slack integration.

--- communi_hub/slack_connector.py
+++ communi_hub/slack_connector.py
@@ -1,11 +1,20 @@
-import sys
+import os
 import logging
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
 
 class SlackConnector:
     def __init__(self) -> None:
         logging.basicConfig(level=logging.DEBUG)
-        client = WebClient()
+        self.client = WebClient(token=os.environ['SLACK_API_KEY'])
 
-    def send_message(self, message, receiver_name, receiver_surname) -> bool:
-        return 'Slack message sent: ' + message + " receiver_name: " + receiver_name + " receiver_surname: " + receiver_surname
+    def send_message(self, message) -> bool:
+        try:
+            response = self.client.chat_postMessage(
+                channel='#general',
+                text=message)
+            return 'Sent from slack' + response["message"]["text"]
+        except SlackApiError as e:
+            print(f"Got an error: {e.response['error']}")
+            return "Unable to send a message on slack"
